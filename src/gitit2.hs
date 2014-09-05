441657c540a274477a92160733e7e76d0aed5282
Freiric Barral <freiric@gmail.com>
2014-09-05 22:58:34 +0200

    added github auth, split configuration in several files, split gitit2.hs in several files

--- src/gitit2.hs
+++ src/gitit2.hs
@@ -1,14 +1,14 @@
 {-# LANGUAGE QuasiQuotes, TemplateHaskell, MultiParamTypeClasses, TypeFamilies,
     OverloadedStrings #-}
 import Network.Gitit2
-import Network.Socket hiding (Debug)
 import Yesod
 import Yesod.Static
 import Yesod.Auth
 import Yesod.Auth.BrowserId
+import Yesod.Auth.GoogleEmail
+import Yesod.Auth.OAuth2.Github
 import Network.Wai.Handler.Warp
 import Data.FileStore
-import Data.Yaml
 import Control.Applicative
 import Text.Pandoc
 import qualified Text.Pandoc.UTF8 as UTF8
@@ -21,15 +21,21 @@ import System.IO
 import System.Exit
 import Data.Text (Text)
 import qualified Data.Text as T
-import Prelude hiding (catch)
+import Prelude
 import Control.Exception (catch, SomeException)
 import qualified Data.Set as Set
 import Paths_gitit2 (getDataFileName)
 import qualified Network.HTTP.Conduit as HC
 -- TODO only for samplePlugin
 import Data.Generics
+import Data.Yaml
 
-data Master = Master { getGitit    :: Gitit
+import Config
+import Error
+import ArgParser
+
+data Master = Master { settings :: FoundationSettings
+                     , getGitit    :: Gitit
                      , maxUploadSize :: Int
                      , getStatic   :: Static
                      , httpManager :: HC.Manager
@@ -75,7 +81,7 @@ instance Yesod Master where
   maximumContentLength x _ = Just $ fromIntegral $ maxUploadSize x
 
   -- needed for BrowserId - can we set it form config or request?
-  approot = ApprootStatic "http://localhost:3000"
+  approot = ApprootMaster $ appRoot . settings
 
 instance YesodAuth Master where
   type AuthId Master = Text
@@ -84,7 +90,12 @@ instance YesodAuth Master where
   loginDest _ = RootR
   logoutDest _ = RootR
 
-  authPlugins _ = [ authBrowserId def ]
+  authPlugins master =
+      let githubAuthPlugin = case gitHub $ settings master of
+                Just githubSetting -> [oauth2Github (clientId githubSetting) (clientSecret githubSetting)]
+                _ ->  []
+      in [ authBrowserId def
+      , authGoogleEmail ] ++ githubAuthPlugin
 
   maybeAuthId = lookupSession "_ID"
 
@@ -127,108 +138,6 @@ getMessagesR = do
       <p.message>#{msg}
     |]
 
--- | Ready collection of common mime types. (Copied from
--- Happstack.Server.HTTP.FileServe.)
-mimeTypes :: M.Map String ContentType
-mimeTypes = M.fromList
-        [("xml","application/xml")
-        ,("xsl","application/xml")
-        ,("js","text/javascript; charset=UTF-8")
-        ,("html","text/html; charset=UTF-8")
-        ,("htm","text/html; charset=UTF-8")
-        ,("css","text/css; charset=UTF-8")
-        ,("gif","image/gif")
-        ,("jpg","image/jpeg")
-        ,("png","image/png")
-        ,("txt","text/plain; charset=UTF-8")
-        ,("doc","application/msword")
-        ,("exe","application/octet-stream")
-        ,("pdf","application/pdf")
-        ,("zip","application/zip")
-        ,("gz","application/x-gzip")
-        ,("ps","application/postscript")
-        ,("rtf","application/rtf")
-        ,("wav","application/x-wav")
-        ,("hs","text/plain; charset=UTF-8")]
-
-data Conf = Conf { cfg_port             :: Int
-                 , cfg_listen_address   :: String
-                 , cfg_repository_path  :: FilePath
-                 , cfg_repository_type  :: Text
-                 , cfg_page_extension   :: FilePath
-                 , cfg_default_format   :: Text
-                 , cfg_static_dir       :: FilePath
-                 , cfg_mime_types_file  :: Maybe FilePath
-                 , cfg_use_mathjax      :: Bool
-                 , cfg_feed_days        :: Integer
-                 , cfg_feed_minutes     :: Integer
-                 , cfg_pandoc_user_data :: Maybe FilePath
-                 , cfg_use_cache        :: Bool
-                 , cfg_cache_dir        :: FilePath
-                 , cfg_front_page       :: Text
-                 , cfg_help_page        :: Text
-                 , cfg_max_upload_size  :: String
-                 , cfg_latex_engine     :: Maybe FilePath
-                 }
-
--- | Read a file associating mime types with extensions, and return a
--- map from extensions to types. Each line of the file consists of a
--- mime type, followed by space, followed by a list of zero or more
--- extensions, separated by spaces. Example: text/plain txt text
-readMimeTypesFile :: FilePath -> IO (M.Map String ContentType)
-readMimeTypesFile f = catch
-  ((foldr go M.empty . map words . lines) `fmap` readFile f)
-  handleMimeTypesFileNotFound
-     where go []     m = m  -- skip blank lines
-           go (x:xs) m = foldr (\ext -> M.insert ext $ B.pack x) m xs
-           handleMimeTypesFileNotFound :: SomeException -> IO (M.Map String ContentType)
-           handleMimeTypesFileNotFound e = do
-             warn $ "Could not parse mime types file.\n" ++ show e
-             return mimeTypes
-
-parseConfig :: Object -> Parser Conf
-parseConfig o = Conf
-  <$> o .:? "port" .!= 3000
-  <*> o .:? "listen_address" .!= "0.0.0.0"
-  <*> o .:? "repository_path" .!= "wikidata"
-  <*> o .:? "repository_type" .!= "git"
-  <*> o .:? "page_extension" .!= ".page"
-  <*> o .:? "default_format" .!= "markdown"
-  <*> o .:? "static_dir" .!= "static"
-  <*> o .:? "mime_types_file"
-  <*> o .:? "use_mathjax" .!= False
-  <*> o .:? "feed_days" .!= 14
-  <*> o .:? "feed_minutes" .!= 15
-  <*> o .:? "pandoc_user_data"
-  <*> o .:? "use_cache" .!= False
-  <*> o .:? "cache_dir" .!= "cache"
-  <*> o .:? "front_page" .!= "Front Page"
-  <*> o .:? "help_page" .!= "Help"
-  <*> o .:? "max_upload_size" .!= "1M"
-  <*> o .:? "latex_engine"
-
-readNumber :: String -> Maybe Int
-readNumber x = case reads x of
-                    ((n,""):_) -> Just n
-                    _          -> Nothing
-
-readSize :: String -> Maybe Int
-readSize x =
-  case reverse x of
-       ('K':xs) -> (* 1000) <$> readNumber (reverse xs)
-       ('M':xs) -> (* 1000000) <$> readNumber (reverse xs)
-       ('G':xs) -> (* 1000000000) <$> readNumber (reverse xs)
-       _        -> readNumber x
-
-err :: Int -> String -> IO a
-err code msg = do
-  hPutStrLn stderr msg
-  exitWith $ ExitFailure code
-  return undefined
-
-warn :: String -> IO ()
-warn msg = hPutStrLn stderr msg
-
 
 -- TODO test
 samplePlugin :: Plugin Master
@@ -240,13 +149,8 @@ samplePlugin = Plugin $ \wp -> do
 
 main :: IO ()
 main = do
-  settingsFile <- getDataFileName "settings.yaml"
-  -- TODO allow command line specification of settings file
-  -- let the data file provide defaults
-  res <- decodeEither `fmap` B.readFile settingsFile
-  conf <- case res of
-             Left e  -> err 3 $ "Error reading configuration file.\n" ++ e
-             Right x -> parseMonad parseConfig x
+  res <- readArgs "settings.yaml"
+  conf <- parseMonad parseConfig res
   let repopath = cfg_repository_path conf
   repoexists <- doesDirectoryExist repopath
   fs <- case T.toLower (cfg_repository_type conf) of
@@ -256,60 +160,27 @@ main = do
              x           -> err 13 $ "Unknown repository type: " ++ T.unpack x
 
   st <- staticDevel $ cfg_static_dir conf
-  mimes <- case cfg_mime_types_file conf of
-                Nothing -> return mimeTypes
-                Just f  -> readMimeTypesFile f
-
-  -- open the requested interface
-  sock <- socket AF_INET Stream defaultProtocol
-  setSocketOption sock ReuseAddr 1
-  device <- inet_addr $ cfg_listen_address conf
-  bindSocket sock $ SockAddrInet (toEnum (cfg_port conf)) device
-  listen sock 10
-
-  format <- case readPageFormat (cfg_default_format conf) of
-                  Just f  -> return f
-                  Nothing -> err 11 $ "Unknown default format: " ++
-                                   T.unpack (cfg_default_format conf)
   maxsize <- case readSize (cfg_max_upload_size conf) of
                   Just s  -> return s
                   Nothing -> err 17 $ "Could not read size: " ++
                                       cfg_max_upload_size conf
 
-  -- clear cache
-  when (cfg_use_cache conf) $ do
-    let cachedir = cfg_cache_dir conf
-    exists <- doesDirectoryExist cachedir
-    when exists $ removeDirectoryRecursive cachedir
-
-  let settings = setPort (cfg_port conf) defaultSettings
-  let gconfig = GititConfig{ mime_types = mimes
-                           , default_format = format
-                           , repository_path = cfg_repository_path conf
-                           , page_extension = cfg_page_extension conf
-                           , static_path = cfg_static_dir conf
-                           , use_mathjax = cfg_use_mathjax conf
-                           , feed_days  = cfg_feed_days conf
-                           , feed_minutes  = cfg_feed_minutes conf
-                           , pandoc_user_data = cfg_pandoc_user_data conf
-                           , use_cache = cfg_use_cache conf
-                           , cache_dir = cfg_cache_dir conf
-                           , front_page = cfg_front_page conf
-                           , help_page = cfg_help_page conf
-                           , latex_engine = cfg_latex_engine conf
-                           }
+  gconfig <- gititConfigFromConf conf
 
   unless repoexists $ initializeRepo gconfig fs
 
-  let runner = runSettingsSocket settings sock
   man <- HC.newManager HC.conduitManagerSettings
-  runner =<< toWaiApp
-      (Master (Gitit{ config = gconfig
+  let settings = setPort (cfg_port conf) defaultSettings
+  print (cfg_port conf)
+  print (foundationSettingsFromConf conf)
+  runSettings settings =<< toWaiApp
+       (Master (foundationSettingsFromConf conf)
+               Gitit{ config = gconfig
                     , filestore = fs
-                    })
-              maxsize
-              st
-              man)
+                    }
+               maxsize
+               st
+               man)
 
 initializeRepo :: GititConfig -> FileStore -> IO ()
 initializeRepo gconfig fs = do
@@ -349,3 +220,16 @@ initializeRepo gconfig fs = do
     $ header ++ helpcontentsInitial ++ "\n\n" ++ helpcontentsMarkup
   create fs "Gitit User’s Guide.page" auth "User’s guide (README)"
     $ header ++ usersguidecontents
+
+readSize :: String -> Maybe Int
+readSize x =
+  case reverse x of
+       ('K':xs) -> (* 1000) <$> readNumber (reverse xs)
+       ('M':xs) -> (* 1000000) <$> readNumber (reverse xs)
+       ('G':xs) -> (* 1000000000) <$> readNumber (reverse xs)
+       _        -> readNumber x
+
+readNumber :: String -> Maybe Int
+readNumber x = case reads x of
+                    ((n,""):_) -> Just n
+                    _          -> Nothing
